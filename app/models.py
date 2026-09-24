50358fb916ae9bd080a9aeab9b5847c761d2be75
lucaspass12869-create <lucaspass12869@gmail.com>
2026-09-23 21:12:30 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,35 +1,43 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, Float, Text
 from sqlalchemy.orm import relationship
-from.database import Base
+from app.database import Base
 
-project_technology = Table('project_technology', Base.metadata,
-    Column('project_id', Integer, ForeignKey('projects.id')),
-    Column('technology_id', Integer, ForeignKey('technologies.id'))
+project_technology = Table(
+    "project_technology",
+    Base.metadata,
+    Column("project_id", Integer, ForeignKey("projects.id")),
+    Column("technology_id", Integer, ForeignKey("technologies.id"))
 )
 
 class Profile(Base):
     __tablename__ = "profiles"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    projects = relationship("Project", back_populates="owner")
+    bio = Column(Text)
+    github_url = Column(String(255))
+    projects = relationship("Project", back_populates="owner", cascade="all, delete")
 
 class Technology(Base):
     __tablename__ = "technologies"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    name = Column(String(50), nullable=False, unique=True)
 
 class Project(Base):
     __tablename__ = "projects"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    link = Column(String)
+    title = Column(String(150), nullable=False)
+    description = Column(Text, nullable=False)
+    repo_url = Column(String(255), nullable=False)
     profile_id = Column(Integer, ForeignKey("profiles.id"), nullable=False)
+    stars = Column(Integer, default=0)
+    average_rating = Column(Float, default=0.0)
     owner = relationship("Profile", back_populates="projects")
-    feedbacks = relationship("Feedback", back_populates="project")
+    technologies = relationship("Technology", secondary=project_technology)
+    feedbacks = relationship("Feedback", back_populates="project", cascade="all, delete")
 
 class Feedback(Base):
     __tablename__ = "feedbacks"
     id = Column(Integer, primary_key=True, index=True)
-    comment = Column(String, nullable=False)
+    note = Column(Integer, nullable=False)
+    comments = Column(Text, nullable=True)
     project_id = Column(Integer, ForeignKey("projects.id"), nullable=False)
     project = relationship("Project", back_populates="feedbacks")
